ed639cf95156921c97f76ada6044a1ea77655caf
Umar Hussain <u7m8a6r@gmail.com>
2025-07-03 13:25:39 +0100

    Set basename for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Little-lemon-booking-table">
     <App />
   </BrowserRouter>
 );
